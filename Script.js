890e8bade3d1f4e8e0023f170a7b2743a4b56999
Ethel3 <akrasiethel4@gmail.com>
2022-07-16 15:56:18 +0000

    button

--- Script.js
+++ Script.js
@@ -1,20 +1,8 @@
-// let bonusPoints = 50
-// bonusPoints = bonusPoints + 50
-
-// bonusPoints = bonusPoints - 75
-// bonusPoints = bonusPoints + 45
-
-// console.log(bonusPoints)
-
-
 let bonusPoints = 50
-console.log(bonusPoints)
-
 bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
 
 bonusPoints = bonusPoints - 75
+bonusPoints = bonusPoints + 45
+
 console.log(bonusPoints)
 
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)

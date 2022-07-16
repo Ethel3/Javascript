9ebaa0567d119883277c4a9265c6fd298c517b79
Ethel3 <akrasiethel4@gmail.com>
2022-07-16 21:43:34 +0000

    strings

--- Script.js
+++ Script.js
@@ -1,14 +1,6 @@
 // let count = 5
 // count = count + 1
 // console.log(count)
-let countEl = document.getElementById("count-el");
-let count = 0;
-
-function increment() {
-  count = count + 1;
-  countEl.innerText = count;
-}
-
-function save() {
-  console.log(count);
-}
+// let countEl = document.getElementById("count-el");
+let username = "Bella"
+console.log(username)

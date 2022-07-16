3122344df0489b17bcf60efde4563e28ca048361
Ethel3 <akrasiethel4@gmail.com>
2022-07-16 16:51:01 +0000

    funtion

--- Script.js
+++ Script.js
@@ -1,18 +1,18 @@
-// let lap1 = 34
-// let lap2 = 30
-// let lap3 = 20
-//  function lapTime() {
-//     let totalTime = lap1 + lap2 + lap3
-//     console.log(totalTime)
-//  }
-//  lapTime()
+let lap1 = 34
+let lap2 = 30
+let lap3 = 20
+ function lapTime() {
+    let totalTime = lap1 + lap2 + lap3
+    console.log(totalTime)
+ }
+ lapTime()
     
-let age1 = 14
-let age2 = 25
-let age3 = 45
+// let age1 = 14
+// let age2 = 25
+// let age3 = 45
 
-function sumAge(){
-    console.log(age1 + age2 + age3)
-}
-sumAge()
+// function sumAge(){
+//     console.log(age1 + age2 + age3)
+// }
+// sumAge()
 

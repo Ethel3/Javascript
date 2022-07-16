6ab453235e796dc66ee4038257d961040ac38969
Ethel3 <akrasiethel4@gmail.com>
2022-07-16 15:37:42 +0000

    Assigning variables

--- Script.js
+++ Script.js
@@ -1,6 +1,20 @@
-let myAge = 12
-let humanDog = 7
+// let bonusPoints = 50
+// bonusPoints = bonusPoints + 50
 
-let myDogAge = myAge * humanDog
+// bonusPoints = bonusPoints - 75
+// bonusPoints = bonusPoints + 45
 
-console.log(myDogAge)
+// console.log(bonusPoints)
+
+
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)

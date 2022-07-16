b60b1300584a9cbec2913e511fe3b8505f5c6a49
Ethel3 <akrasiethel4@gmail.com>
2022-07-16 19:35:41 +0000

    update

--- Script.js
+++ Script.js
@@ -1,13 +1,13 @@
 // let count = 5
 // count = count + 1
 // console.log(count)
-let countEl = document.getElementById("count-el");
-console.log(countEl);
+let countEl = document.getElementById("count-el")
 
-let count = 1;
+console.log(countEl)
 
-function increment() {
-  // console.log("clicked")
-  count = count + 1;
-  console.log(count);
+let count = 0
+
+function increment () {
+    count = count + 1
+    console.log(count)
 }

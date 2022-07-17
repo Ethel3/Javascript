8b6e882eba8c1f905bb6c90a45dc7446c2e21853
Ethel3 <akrasiethel4@gmail.com>
2022-07-17 19:16:51 +0000

    welcome

--- Script.js
+++ Script.js
@@ -2,23 +2,9 @@
 // count = count + 1
 // console.log(count)
 // let countEl = document.getElementById("count-el");
-
+let welcomeEl = document.getElementById("welcome-el")
 
 let name = "Ethel"
-let greeting = "Hello,  my name is "
+let greeting = "Welcome back "
 
-let myGreeting = greeting  + name
-console.log(myGreeting)
-
-let age1 = 4
-let age2 = 5
-console.log(4 + 5)
-
-let dog1 = "2"
-let dog2 = "4"
-console.log("2" + "4")
-
-let lap1 = "5"
-let lap2 = 1
-console.log("5" + 1)
-
+welcomeEl.innerText = greeting + name

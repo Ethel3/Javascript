f17380efe532f5a2b631c01409e4b07240b2c77d
Ethel3 <akrasiethel4@gmail.com>
2022-07-17 19:43:43 +0000

    leaderboard

---
+++ leaderboard.js
@@ -0,0 +1,9 @@
+let welcomeEl = document.getElementById("welcome-el")
+
+let name = "Ethel"
+let greeting = "Welcome back "
+
+welcomeEl.innerText = greeting + name
+
+welcomeEl.innerText = welcomeEl.innerText + "👋"
+// welcomeEl.innerText += "👋"

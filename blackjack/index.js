782d804fe764e51ac2bcab60ee893e1270801c67
Ethel3 <akrasiethel4@gmail.com>
2022-07-22 13:30:08 +0000

    If else conditions

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,13 +1,21 @@
-let firstCard = 11
-let secondCard = 12
-let sum = firstCard + secondCard
+// let firstCard = 11
+// let secondCard = 12
+// let sum = firstCard + secondCard
 
-if (sum < 21) {
-    console.log("Draw new cards" + "🙂")
+// if (sum < 21) {
+//     console.log("Draw new cards" + "🙂")
+// }
+// else if (sum === 21) {
+//     console.log("Comgrats!!" + "🥳")
+// }
+// else if(sum > 21) {
+//     console.log("You're out of the game!" + "😭")
+// }
+
+let age = 20
+if (age < 21){
+    console.log("You're under age ")
 }
-else if (sum === 21) {
-    console.log("Comgrats!!" + "🥳")
-}
-else if (sum > 21) {
-    console.log("You're out of the game" + "😭")
+else{
+    console.log("You're of age")
 }

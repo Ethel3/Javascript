a38eff5b0514785fc54733e28542e0c958f3a9e9
Ethel3 <akrasiethel4@gmail.com>
2022-07-22 12:51:43 +0000

    deleted files

--- index.js
+++
@@ -1,8 +0,0 @@
-let errorParagraph = document.getElementById("error")
-console.log(errorParagraph)
-
-function purchase() {
-    console.log("buttom clicked")
-    errorParagraph.textContent = "Something went wrong"
-    
-}
--- leaderboard.js
+++
@@ -1,9 +0,0 @@
-let welcomeEl = document.getElementById("welcome-el")
-
-let name = "Ethel"
-let greeting = "Welcome back "
-
-welcomeEl.innerText = greeting + name
-
-welcomeEl.innerText = welcomeEl.innerText + "👋"
-// welcomeEl.innerText += "👋"

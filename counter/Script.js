c4a2284c2405c28f8d081c49294319557bece86a
Ethel3 <akrasiethel4@gmail.com>
2022-07-22 12:52:35 +0000

    counter update

---
+++ counter/Script.js
@@ -0,0 +1,25 @@
+// let count = 5
+// count = count + 1
+// console.log(count)
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el");
+let count = 0
+
+console.log(saveEl)
+
+function increment (){
+    count = count + 1
+    countEl.textContent = count
+}
+
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent +=countStr
+    countEl.textContent = 0
+    count = 0
+    
+}
+
+console.log("Let's count people on the subway!")
+
+

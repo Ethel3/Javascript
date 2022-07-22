5753a1d71fbd73a14e2df8cd8ba0ddf0083df61c
Ethel3 <akrasiethel4@gmail.com>
2022-07-22 14:43:56 +0000

    condition

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,23 +1,25 @@
-// let firstCard = 11
-// let secondCard = 12
-// let sum = firstCard + secondCard
+let firstCard = 11
+let secondCard = 16
+let sum = firstCard + secondCard
+let hasBlackJack = false
 
-// if (sum < 21) {
-//     console.log("Draw new cards" + "🙂")
-// }
-// else if (sum === 21) {
-//     console.log("Comgrats!!" + "🥳")
-// }
-// else if(sum > 21) {
-//     console.log("You're out of the game!" + "😭")
-// }
+if (sum < 21) {
+    console.log("Draw new cards" + "🙂")
+}
+else if (sum === 21) {
+    console.log("Comgrats!!" + "🥳")
+    let hasBlackJack = true
+}
+else if(sum > 21) {
+    console.log("You're out of the game!" + "😭")
+}
 
-let age = 150
-if (age < 100){
-    console.log("You're not eligible! ")
-}
-else if(age == "100"){
-    console.log("Here is your Birthday Card!!")
-}else {
-    console.log("You're not eligible! You already have a Card")
-}
+// let age = 150
+// if (age < 100){
+//     console.log("You're not eligible! ")
+// }
+// else if(age == "100"){
+//     console.log("Here is your Birthday Card!!")
+// }else {
+//     console.log("You're not eligible! You already have a Card")
+// }

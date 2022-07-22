58b6c5ef9e4daf8f213855119d80667ba7fc2d39
Ethel3 <akrasiethel4@gmail.com>
2022-07-22 13:40:29 +0000

    if/else if/else

--- blackjack/index.js
+++ blackjack/index.js
@@ -12,10 +12,12 @@
 //     console.log("You're out of the game!" + "😭")
 // }
 
-let age = 20
-if (age < 21){
-    console.log("You're under age ")
+let age = 150
+if (age < 100){
+    console.log("You're not eligible! ")
 }
-else{
-    console.log("You're of age")
+else if(age === 100){
+    console.log("Here is your Birthday Card!!")
+}else {
+    console.log("You're not eligible")
 }

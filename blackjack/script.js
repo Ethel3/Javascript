e9ee5555d03d24ab1700164f2cedc9f0fafea8c5
Ethel3 <akrasiethel4@gmail.com>
2022-07-25 19:16:53 +0000

    for loops

--- blackjack/script.js
+++ blackjack/script.js
@@ -1,13 +1,16 @@
-let techCareers = [
-    "Software Engineer",
-    "Frontend Developer",
-    "UI/UX designer",
-    "Backend Developer",
-    "Cyber security personnel",
-]
-// console.log(techCareers)
-console.log(techCareers[4])
+// let techCareers = [
+//     "Software Engineer",
+//     "Frontend Developer",
+//     "UI/UX designer",
+//     "Backend Developer",
+//     "Cyber security personnel",
+// ]
+// // console.log(techCareers)
+// console.log(techCareers[4])
 
 
-console.log(techCareers.length)
+// console.log(techCareers.length)
+for( let count =1; count < 21; count+=2){
+    console.log(count)
+}
  

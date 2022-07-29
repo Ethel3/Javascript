01fef9405d699c9bf487a3c4ce6fefdc526de895
Ethel3 <akrasiethel4@gmail.com>
2022-07-29 21:46:46 +0000

    completed getrandomNumber

--- blackjack/index.js
+++ blackjack/index.js
@@ -13,11 +13,18 @@ let cardsEl = document.getElementById("cards-el")
 
 
 function getRandomCard(){
-    return Math.floor(Math.random()*13) + 1
+    let  randomNumber = Math.floor ( Math.random()*13) + 1
+    if (randomNumber > 10){
+        return 10
+    } else if (randomNumber === 1){
+        return 11
+    }else {
+        return randomNumber
+    }
 }
 
 function startGame(){
-    renderGame
+    renderGame()
 }
 
 

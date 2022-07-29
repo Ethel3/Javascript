3f1f6556b52ab354e1b58872f054c5b53f36324f
Ethel3 <akrasiethel4@gmail.com>
2022-07-29 20:57:26 +0000

    update on if condition

--- blackjack/script.js
+++ blackjack/script.js
@@ -31,9 +31,24 @@
 //     console.log(messages[i])
 // }
 
-let sentence = ["Hello","my","name", "is","Ethel"]
-let greetingEl = document.getElementById("greeting-el")
+// let sentence = ["Hello","my","name", "is","Ethel"]
+// let greetingEl = document.getElementById("greeting-el")
 
-for (let i =0; i<sentence.length; i++){
-    greetingEl.textContent += sentence[i] +" "
+// for (let i =0; i<sentence.length; i++){
+//     greetingEl.textContent += sentence[i] +" "
+// }
+
+let player1Time = 102
+let player2Time = 107
+
+function getFastestRaceTime(){
+    if (player1Time < player2Time){
+        return player1Time
+    }else if(player2Time < player1Time){
+        return player2Time
+    }else{
+        return player1Time
+    }
 }
+let fastestRace = getFastestRaceTime()
+console.log(fastestRace)

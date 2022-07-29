aa159abc9be5fd8fc4af76d45bab54d1c3795de5
Ethel3 <akrasiethel4@gmail.com>
2022-07-29 21:22:31 +0000

    Math.floor

--- blackjack/script.js
+++ blackjack/script.js
@@ -60,6 +60,6 @@ function getFastestRaceTime(){
 // console.log(totalTime)
 
 //GENERATING RANDOM NUMBERS 
-let randomNumber = Math.random()
+let randomNumber = Math.floor(3.557465) 
 
 console.log(randomNumber)

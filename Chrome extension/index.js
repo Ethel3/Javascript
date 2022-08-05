ba399fc5956888310b3c364260c904d6790e5663
Ethel3 <akrasiethel4@gmail.com>
2022-08-05 16:14:48 +0000

    update

--- Chrome extension/index.js
+++ Chrome extension/index.js
@@ -1,6 +1,6 @@
 let inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function(){
-    console.log("Button clicked from addEventListener")
+    console.log("Button clicked!")
 
 })

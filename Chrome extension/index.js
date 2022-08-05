b7af2ee0196b1b1610da5a7c7ac2f5acf7f43e57
Ethel3 <akrasiethel4@gmail.com>
2022-08-05 18:35:08 +0000

    creating elements

--- Chrome extension/index.js
+++ Chrome extension/index.js
@@ -13,5 +13,9 @@ inputBtn.addEventListener("click", function(){
 
 for(let i = 0; i < myLeads.length; i++){
     console.log(myLeads[i])
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>" + " "
+    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>" + " "
+
+   const li =  document.createElement("li")
+   li.textContent = myLeads[i]
+   ulEl.append(li)
 }

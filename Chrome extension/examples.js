5f4ce1ce9edbf6af98d966988286155259641e82
Ethel3 <akrasiethel4@gmail.com>
2022-08-05 18:17:26 +0000

    updated

--- Chrome extension/examples.js
+++ Chrome extension/examples.js
@@ -1,3 +1,7 @@
 const container = document.getElementById("container")
 
 container.innerHTML = "<button onclick = 'buy()'> Buy!</button>"
+
+function buy(){
+    container.innerHTML = "<p>Thank you for buying!</p>"
+}

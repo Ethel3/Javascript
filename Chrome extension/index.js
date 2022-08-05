e173bc32130d3ff166ee68f0a36ebfeb8fdbb014
Ethel3 <akrasiethel4@gmail.com>
2022-08-05 17:06:14 +0000

    for loop

--- Chrome extension/index.js
+++ Chrome extension/index.js
@@ -1,7 +1,9 @@
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.examplelead.com"]
 const inputEl = document.getElementById("input-el")
-
 const inputBtn = document.getElementById("input-btn")
+
+const ulEl = document.getElementById("ul-el")
+console.log(ulEl)
 
 inputBtn.addEventListener("click", function(){
    myLeads.push(inputEl.value)

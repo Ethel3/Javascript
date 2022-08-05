438dbbdcd7d0710aef5cf020cdf23c922e5a17ba
Ethel3 <akrasiethel4@gmail.com>
2022-08-05 16:43:58 +0000

    updated

--- Chrome extension/index.js
+++ Chrome extension/index.js
@@ -4,6 +4,7 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function(){
-    console.log("Button clicked!")
+   myLeads.push("www.awesomelead.com")
+   console.log(myLeads)
 
 })

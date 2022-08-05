da992db8e0c316d80f11fdbabf00f6e02a0a9460
Ethel3 <akrasiethel4@gmail.com>
2022-08-05 15:22:26 +0000

    addEventListener

--- Chrome extension/index.js
+++ Chrome extension/index.js
@@ -1,7 +1,3 @@
-function saveLead(){
-    console.log("Button click from onclick attribute")
-}
-
 let inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function(){

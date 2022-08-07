d645b9d09e15d4666adee525b2f101474eb93600
Ethel3 <akrasiethel4@gmail.com>
2022-08-07 22:03:17 +0000

    Template strings

--- Chrome extension/index.js
+++ Chrome extension/index.js
@@ -14,8 +14,15 @@ function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
     // console.log(myLeads[i]);
-    listItems += "<li><a target='_blank' href='"+ myLeads[i] +"'>" + myLeads[i] + "</a></li>" + " ";
-    console.log(listItems)
+    // listItems += "<li><a target='_blank' href='"+ myLeads[i] +"'>" + myLeads[i] + "</a></li>" + " ";
+    listItems += `
+    <li>
+    <a target='_blank' href='${myLeads[i]}'>
+    ${myLeads[i]}
+    </a>
+    </li>
+    `
+    
   }
 
   ulEl.innerHTML = listItems;

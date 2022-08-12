44259f323820d322999bfea6c1a65ce97e0bc62d
Ethel3 <akrasiethel4@gmail.com>
2022-08-12 18:27:16 +0000

    Getting leads from localStorage

--- Chrome extension/index.js
+++ Chrome extension/index.js
@@ -4,6 +4,9 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+console.log(leadsFromLocalStorage)
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
@@ -12,7 +15,7 @@ inputEl.value = ""
 localStorage.setItem("myLeads", JSON.stringify(myLeads))
   renderLeads()
 
-  console.log(localStorage.getItem("myLeads"))
+  // console.log(localStorage.getItem("myLeads"))
 });
 function renderLeads() {
   let listItems = "";

908be7f74e14598e5e954eb861bba5c47b3601c8
Ethel3 <akrasiethel4@gmail.com>
2022-08-12 18:32:48 +0000

    Truthy and falsy values

--- Chrome extension/script.js
+++ Chrome extension/script.js
@@ -1,28 +1,16 @@
-// let box = document.getElementById("box")
+// const recipient = "Ethel"
+// const sender = "Akosua"
+// const email =`
+// Hey ${recipient}! 
+// How are you doing? 
+//  ${sender}
+//  `
 
-// box.addEventListener("click", function(){
-//     console.log("I want to open the box!")
-// })
+// console.log(email)
 
-// const basePrice = 520
-// const discount = 120
-// let shippingCost = 12
-// let shippingTime = "5 -12 days"
-
-// shippingCost = 15
-// shippingTime = "7 - 15 days"
-
-// const fullPrice = basePrice - discount + shippingCost
-
-// console.log("Total cost: " + fullPrice + ". It will arrive in the 7 - 15 days")
-
-
-const recipient = "Ethel"
-const sender = "Akosua"
-const email =`
-Hey ${recipient}! 
-How are you doing? 
- ${sender}
- `
-
-console.log(email)
+const credits = 0
+if (credits > 0) {
+    console.log("lets play")
+}else{
+    console.log("Sorry, you have no credits")
+}

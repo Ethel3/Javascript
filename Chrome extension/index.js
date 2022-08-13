454dc9e0083800cb507a882d3aedcbf0fee4c95c
Ethel3 <akrasiethel4@gmail.com>
2022-08-13 15:30:39 +0000

    Delete button

--- Chrome extension/index.js
+++ Chrome extension/index.js
@@ -3,15 +3,23 @@ let myLeads = []
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn")
 
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 console.log(leadsFromLocalStorage)
 
 if (leadsFromLocalStorage){
   myLeads = leadsFromLocalStorage
   renderLeads()
 }
+
+deleteBtn.addEventListener("dblclick", function(){
+  console.log("double clicked!")
+  localStorage.clear()
+  myLeads = []
+  renderLeads()
+})
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);

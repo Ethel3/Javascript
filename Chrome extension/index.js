9c7c0100220eedace06e337760b727902fbbe077
Ethel3 <akrasiethel4@gmail.com>
2022-08-13 16:20:03 +0000

    save tab url

--- Chrome extension/index.js
+++ Chrome extension/index.js
@@ -1,6 +1,5 @@
 // chrome://extensions/
 let myLeads = []
-let oldLeads = []
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
@@ -19,7 +18,10 @@ const tabs = [
 ]
 
 tabBtn.addEventListener("click", function(){
-  console.log(tabs[0].url)
+  // console.log(tabs[0].url)
+  myLeads.push(tabs[0].url)
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads)
 })
 
 function render(leads) {

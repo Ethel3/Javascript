15ebfddb24cff4615a1a19f93d787627c1b7fa6b
Ethel Akrasi Akosua <88440439+Ethel3@users.noreply.github.com>
2022-09-19 00:06:35 +0100

    Update examples.js

--- Chrome extension/examples.js
+++ Chrome extension/examples.js
@@ -1,10 +1,10 @@
-// const container = document.getElementById("container")
+const container = document.getElementById("container")
 
-// container.innerHTML = "<button onclick = 'buy()'> Buy!</button>"
+container.innerHTML = "<button onclick = 'buy()'> Buy!</button>"
 
-// function buy(){
-//     container.innerHTML += "<p>Thank you for buying!</p>"
-// }
+function buy(){
+    container.innerHTML += "<p>Thank you for buying!</p>"
+}
 
 
 //parameters are inside the function.
